8b851b4e01dddb763bffc973ae38a49dcd9c1621
hnleee <lhn1455@gmail.com>
2022-08-01 17:59:15 +0900

    [add] interface/IAirdrop.sol for Airdrop.sol

---
+++ contracts/interface/IAirdrop.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract IAirdrop {
+
+    function doAirDrop(address payable [] calldata _address, uint256 _amount, uint256 _ethAmount)  external virtual returns (bool) {
+
+
+    }
+
+    function sendBath(address payable [] calldata _recipients, uint[] calldata _values) external virtual returns (bool) {
+
+    }
+
+}
