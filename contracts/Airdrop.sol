67abbc6ee21d320f2c43c1e3bb346f416f413dc1
hnleee <lhn1455@gmail.com>
2022-08-02 17:59:10 +0900

    [edit] Airdrop.sol

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -2,22 +2,22 @@
 pragma solidity ^0.8.0;
 
 import "./interface/IAirdrop.sol";
-import "./Token.sol";
+import "./AirToken.sol";
 import "../../../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
 contract AirDrop is Ownable, IAirdrop {
 
-    Token public tokenInstance;
+    AirToken public tokenInstance;
 
-    constructor(address payable _tokenAddress) public {
-        tokenInstance = Token(_tokenAddress);
+    constructor(address payable _tokenAddress) {
+        tokenInstance = AirToken(_tokenAddress);
     }
 
     /*
     Airdrop function which take up a array of address, single token amount and eth amount and call the
     transfer function to send the token plus send eth to the address is balance is 0
    */
-    function doAirDrop(address payable[] calldata _address, uint256 _amount, uint256 _ethAmount) onlyOwner external override returns (bool) {
+    function doAirDrop(address payable[] calldata _address, uint256 _amount, uint256 _ethAmount) onlyOwner external returns (bool success) {
         uint256 count = _address.length;
         for (uint256 i = 0; i < count; i++)
         {
@@ -26,11 +26,12 @@ contract AirDrop is Ownable, IAirdrop {
             if((_address[i].balance == 0) && (address(this).balance >= _ethAmount))
             {
                 require(_address[i].send(_ethAmount));
+                return true;
             }
         }
     }
 
-    function sendBath(address payable [] calldata _recipients, uint[] calldata _values) onlyOwner external override returns (bool) {
+    function sendBath(address payable [] calldata _recipients, uint[] calldata _values) onlyOwner external returns (bool success) {
         require(_recipients.length == _values.length);
         for (uint i = 0; i <_values.length; i++) {
             tokenInstance.transfer(_recipients[i], _values[i]);
