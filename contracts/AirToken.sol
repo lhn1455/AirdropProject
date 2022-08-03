fd7779812068d48a7c6ecfb687b6d253114b2234
hnleee <lhn1455@gmail.com>
2022-08-03 14:24:18 +0900

    [edit] import 경로 수정

--- contracts/AirToken.sol
+++ contracts/AirToken.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
 
-import "../../../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "../../../node_modules/@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-import "../../../node_modules/@openzeppelin/contracts/utils/Context.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "../node_modules/@openzeppelin/contracts/utils/Context.sol";
 
 contract AirToken is Context, IERC20, IERC20Metadata {
 
