be46641a1baca6bb7d7670d4a0cb179a42c79dfe
hnleee <lhn1455@gmail.com>
2022-08-04 09:58:05 +0900

    [edit] test수정

--- test/1_AirTokenTest.js
+++ test/1_AirTokenTest.js
@@ -1,20 +1,20 @@
-const { expect } = require("chai");
-const {
-    time,
-    loadFixture,
-} = require("@nomicfoundation/hardhat-network-helpers");
-const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const {loadFixture} = require("@nomicfoundation/hardhat-network-helpers");
 const hre = require("hardhat");
 
 
 describe('AirToken', function() {
      async function deployAirToken() {
-         const [owner, otherAccount] = await hre.ethers.getSigners();
+        const [ deployer, client1, client2, client3 ] = await ethers.getSigners();
+        console.log("Deploying contracts with the account:", deployer.address);
+        console.log("client1 address", client1.address)
+        console.log("client2 address", client2.address)
+        console.log("client3 address", client3.address)
+        
 
         const AirToken = await hre.ethers.getContractFactory("AirToken");
         const airToken = await AirToken.deploy();
 
-        return { airToken, owner, otherAccount };
+        return { airToken, deployer, client1, client2, client3 };
     }
 
 
