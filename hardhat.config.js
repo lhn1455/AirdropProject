6df6d3ddc234e68a52e5487ff0765ea635840a74
hnleee <lhn1455@gmail.com>
2022-08-04 09:57:04 +0900

    [edit] hardhat.config.js rinkeby 네트워크 추가

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,31 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
+
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+ module.exports = {
+  networks: {
+    dev: {
+      url: `http://localhost:8545`,
+      chainId: 1337, // config standard
+    },
+
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.PRI_KEY],
+      // gasPrice: 470000000000,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      rinkeby: process.env.ETHERSCAN_API_KEY
+    },
+  },
   solidity: "0.8.9",
 };
+
