13d675cd7dc46000a73bf7aa93b25942bec00062
hnleee <lhn1455@gmail.com>
2022-08-05 18:26:52 +0900

    [edit] test logic

--- test/2_AirdropTest.js
+++ test/2_AirdropTest.js
@@ -1,35 +1,49 @@
 const {loadFixture} = require("@nomicfoundation/hardhat-network-helpers");
 const hre = require("hardhat");
+const Web3 = require("web3");
+const web3 = new Web3("http://localhost:8545");
 
 
 describe('Airdrop', function() {
-     async function deployAirdrop() {
-        const [ deployer, client1, client2, client3 ] = await ethers.getSigners();
+     async function deployAirdropContractSet() {
+        const [ deployer ] = await ethers.getSigners();
         console.log("Deploying contracts with the account:", deployer.address);
-        console.log("client1 address", client1.address)
-        console.log("client2 address", client2.address)
-        console.log("client3 address", client3.address)
         
-
-        const Airdrop = await hre.ethers.getContractFactory("AirDrop");
         const AirToken = await hre.ethers.getContractFactory("AirToken");
+        const Airdrop = await hre.ethers.getContractFactory("AirDrop");
+        const Address = await hre.ethers.getContractFactory("Address");
+        
+        
     
         const airToken = await AirToken.deploy();
+        const address = await Address.deploy();
         const airdrop = await Airdrop.deploy(airToken.address);
 
-        return { airdrop, airToken, deployer, client1, client2, client3 };
+        return { airdrop, airToken, deployer, address};
     }
 
 
 describe('ceate Airdrop', function () {
 
     it("should be the right name", async function() {
-        const { airdrop, airToken } = await loadFixture(deployAirdrop);
+        const [ deployer ] = await ethers.getSigners();
+        const { airdrop, airToken, address } = await loadFixture(deployAirdropContractSet);
         
+       
         const name = await airToken.name();
         console.log("name : ", name);
-        const address = await airdrop.getAddress();
-        console.log("address :", address);
+        const airdropContract = await airdrop.getAddress();
+
+
+        const balance = await web3.eth.getBalance(airdropContract);
+        console.log("value : " ,  balance);
+
+
+        console.log("airdropContract :", airdropContract);
+
+        const add = await address.getContractAddress();
+        console.log("ClientAddress : " ,  add);
+        
 
     });
 
