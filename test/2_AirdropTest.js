bf9b3bf44559f09344174a871444d909a0033406
hnleee <lhn1455@gmail.com>
2022-08-06 20:06:30 +0900

    [edit] test logic

--- test/2_AirdropTest.js
+++ test/2_AirdropTest.js
@@ -1,7 +1,6 @@
 const {loadFixture} = require("@nomicfoundation/hardhat-network-helpers");
 const hre = require("hardhat");
-const Web3 = require("web3");
-const web3 = new Web3("http://localhost:8545");
+
 
 
 describe('Airdrop', function() {
@@ -10,16 +9,16 @@ describe('Airdrop', function() {
         console.log("Deploying contracts with the account:", deployer.address);
         
         const AirToken = await hre.ethers.getContractFactory("AirToken");
-        const Airdrop = await hre.ethers.getContractFactory("AirDrop");
-        const Address = await hre.ethers.getContractFactory("Address");
+        const AirDrop = await hre.ethers.getContractFactory("AirDrop");
+        const ClientAddress = await hre.ethers.getContractFactory("ClientAddress");
         
         
     
         const airToken = await AirToken.deploy();
-        const address = await Address.deploy();
-        const airdrop = await Airdrop.deploy(airToken.address);
+        const clientAddress = await ClientAddress.deploy();
+        const airDrop = await AirDrop.deploy(airToken.address, clientAddress.address);
 
-        return { airdrop, airToken, deployer, address};
+        return { airDrop, airToken, deployer, clientAddress};
     }
 
 
@@ -27,26 +26,41 @@ describe('ceate Airdrop', function () {
 
     it("should be the right name", async function() {
         const [ deployer ] = await ethers.getSigners();
-        const { airdrop, airToken, address } = await loadFixture(deployAirdropContractSet);
+        const { airDrop, airToken, clientAddress } = await loadFixture(deployAirdropContractSet);
         
        
         const name = await airToken.name();
         console.log("name : ", name);
-        const airdropContract = await airdrop.getAddress();
+        const client = await clientAddress.address;
+        console.log("client :", client);
 
-        const clientAddress = await address.getClientAddress();
-        for (let i =0; i < clientAddress.length; i++){
-            console.log("clientAddress : ", clientAddress[i]);
-        }
+        // const balance = await airToken.balance;
+        // console.log("balance : ", balance);
+
+        const balance = await airDrop.getBalance(airToken.address);
+        console.log("balance : ", balance);
         
 
+        const clientList = await clientAddress.getClientAddress();
+        for (let i =0; i < clientList.length; i++){
+            console.log("clientList : ", clientList[i]);
+        }
+        
+        const count = await clientAddress.getCount();
+        console.log("count : ", count);
 
+        
 
-        console.log("airdropContract :", airdropContract);
+       
+        const clientList2 = await airDrop.getClientList();
+        for (let i =0; i < clientList2.length; i++){
+            console.log("clientListFromAirdop : ", clientList2[i]);
+         
+        }
 
-        const add = await address.getContractAddress();
-        console.log("Address : " ,  add);
-
+        const result = await airToken._transfer(airToken.address,clientList2[2], 10)
+        console.log("result : ", result);
+        console.log("clientList2[2] : ", clientList2[2]);
         
 
     });
