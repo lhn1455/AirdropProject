bd16997da7f968c72574d2c107967f29ea5f955d
hnleee <lhn1455@gmail.com>
2022-08-07 14:22:58 +0900

    [edit] 로직 수정

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,16 +4,27 @@ async function main() {
   const [ deployer ] = await ethers.getSigners();
 
   console.log("Deploying contracts with the account:", deployer.address);
-  // console.log("client1 address", client1.address)
-  // console.log("client2 address", client2.address)
-  // console.log("client3 address", client3.address)
-  
+
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  const AirToken = await ethers.getContractFactory("AirToken");
-  const airToken = await AirToken.deploy();
+  const AirToken = await hre.ethers.getContractFactory("AirToken");
+  const AirDrop = await hre.ethers.getContractFactory("AirDrop");
+  const ClientAddress = await hre.ethers.getContractFactory("ClientAddress");
 
-  console.log("Token address:", airToken.address);
+  const airToken = await AirToken.deploy();
+  const clientAddress = await ClientAddress.deploy();
+  const airDrop = await AirDrop.deploy(airToken.address, clientAddress.address);
+
+  await airDrop.doAirDrop(15);
+  const clientList2 = await airDrop.getClientAddress();
+  for (let i =0; i < clientList2.length; i++){
+      console.log("clientListFromAirdop : ", clientList2[i]);
+      const balances = await airDrop.getBalance(clientList2[i]);
+      console.log("balances : ", balances)
+  }
+  return { airDrop, airToken, deployer, clientAddress};
+
+  
 }
 
 main()
