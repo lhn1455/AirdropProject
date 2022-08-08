b2dbbb15c3603743c677c864fc877b68cc225b75
hnleee <lhn1455@gmail.com>
2022-08-08 18:24:39 +0900

    [edit] interface 수정

--- contracts/interface/IAirdrop.sol
+++ contracts/interface/IAirdrop.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.0;
 
 interface IAirdrop {
 
-    function doAirDrop(address payable [] calldata _address, uint256 _amount, uint256 _ethAmount)  external returns (bool success) ;
+    function doAirDrop(uint256 _amount)  external returns (bool success) ;
     function sendBath(address payable [] calldata _recipients, uint[] calldata _values) external returns (bool success) ;
     function destroyContract (address payable _contract) external;
     function getClientAddress() external view returns (address payable[] memory);
     function getBalance(address payable _address) external view returns (uint);
-}
+} 
