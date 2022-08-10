6edc63537b9670fc94443a33030a7265968c9587
hnleee <lhn1455@gmail.com>
2022-08-10 15:19:07 +0900

    [edit] test logic

--- scripts/deploy_AirDrop_v1.js
+++ scripts/deploy_AirDrop_v1.js
@@ -1,17 +1,17 @@
-
+const hre = require("hardhat");
 const { ethers, upgrades } = require("hardhat");
 
 async function main() {
-    const AirToken = await ethers.getContractFactory("AirToken");
-    const AirDrop = await ethers.getContractFactory("AirDrop");
-    const ClientAddress = await ethers.getContractFactory("ClientAddress");
+    const AirToken = await hre.getContractFactory("AirToken");
+    const AirDrop = await hre.getContractFactory("AirDrop");
+    const ClientAddress = await hre.getContractFactory("ClientAddress");
 
     const airToken = await AirToken.deploy();
     const clientAddress = await ClientAddress.deploy();
-    const airDrop = await AirDrop.deploy(airToken.address, clientAddress.address);
+    // const airDrop = await AirDrop.deploy(airToken.address, clientAddress.address);
     console.log("Deploying Airdrop...");
 
-    const airdrop = await upgrades.deployProxy(airDrop, {
+    const airdrop = await upgrades.deployProxy(AirDrop, [ airToken.address, clientAddress.address ], {
         initializer: "initialize",
     });
 
