a64965d35434342df5551a3f6eb49fd6cba1318b
hnleee <lhn1455@gmail.com>
2022-08-12 19:02:16 +0900

    [edit] AirDrop deploy script

--- scripts/deploy_AirDrop_v1.js
+++ scripts/deploy_AirDrop_v1.js
@@ -1,6 +1,8 @@
 
 const { ethers, upgrades } = require("hardhat");
 
+//오픈제플린-업그레이드 임포트 에러
+
 async function main() {
     const AirToken = await ethers.getContractFactory("AirToken");
     const AirDrop = await ethers.getContractFactory("AirDrop");
